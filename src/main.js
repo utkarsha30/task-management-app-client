2ec1c6664ac2fc934baac4dd93e19ee11b218194
ukshirsa <utkarshakshirsagar4717@gmail.com>
2023-04-26 17:20:52 +0530

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
